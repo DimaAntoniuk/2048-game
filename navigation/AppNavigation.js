5868b91323739b141b7811b4c989b1219facad66
dimonlavron <dima.lavrishin@gmail.com>
2021-01-29 06:49:12 +0200

    menu navigation from game

--- navigation/AppNavigation.js
+++ navigation/AppNavigation.js
@@ -1,11 +1,29 @@
 import { createStackNavigator } from 'react-navigation-stack'
 import MainMenu from '../screens/MainMenu'
 import Grid from '../screens/Grid'
+import React from 'react';
+import { Image, TouchableOpacity } from 'react-native';
 
 const AppNavigation = createStackNavigator(
     {
         MainMenu: { screen: MainMenu },
-        Grid: { screen: Grid }
+        Grid: { 
+            screen: Grid,
+            navigationOptions: ({navigation}) => ({
+                gestureEnabled: false,
+                headerLeft: () => (
+                    <TouchableOpacity
+                        onPress={() => {navigation.goBack()}}
+                    >
+                        <Image
+                            style={{width: 35, height: 35, marginLeft: 10}}
+                            source={require('../assets/hamburger.png')}
+                        />  
+                    </TouchableOpacity>
+                    
+                ),
+            })
+        }
     },
     {
         initialRouteName: 'MainMenu',
